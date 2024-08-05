f36737f9e35785af2d84ad56771cec0705d7611a
practicante <xaviedunavi@gmail.com>
2024-08-05 11:43:21 -0500

    Print-server-mini

--- frontend/src/components/ForgotPasswordModal.js
+++ frontend/src/components/ForgotPasswordModal.js
@@ -7,7 +7,6 @@ const ForgotPasswordModal = ({ onClose }) => {
       <div className="modal-container">
         <h2>¿Olvidaste tu contraseña?</h2>
         <p>Acércate a la área de TICs o ponte en contacto con el ingeniero a cargo.</p>
-        <p>Ing. Alejandro</p>
         <button onClick={onClose}>Cerrar</button>
       </div>
     </div>
